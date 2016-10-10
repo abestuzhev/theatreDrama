482b9335d2fc5065b7b5aec85a28d6f29967eb2d
Aleksey <aleksey.bestuzhev@gmail.com>
2016-10-10 06:43:10 +0300

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
+    $('.top-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        adaptiveHeight: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        cssEase: 'linear'
+    });
 
+    // $('#anchor-tag').lightGallery();
 });
 
 $(window).bind('load', function() {
 
     $('.js-dotted').dotdotdot();
 });
+
+lightGallery(document.getElementById('anchor-tag'));
