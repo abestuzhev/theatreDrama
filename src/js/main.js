bc0894717c57daa642dd4ede4a86eb1efe0637aa
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2016-10-14 15:28:20 +0300

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
 
-    //focus
-    // $( ".c-search_field input" ).focus(function() {
-    //     $('.c-search_autocomplete').toggleClass('is-visible');
-    //     $('.c-search_autocomplete').blur();
-    // });
-
     $('#header-search').focus(function() {
         $('.c-search_autocomplete').show();
         $(document).bind('focusin.c-search_autocomplete click.c-search_autocomplete',function(e) {
@@ -103,12 +97,21 @@ $(document).ready(function() {
         $('.header').css("padding-top","82px");
         $('#CecutientOff').css("display","block");
         $('.header-title_season-number').css("color","#000");
-        // $('.header-title_season-text').css("background-color","#000");
+        $('.header-title_season-text').css("background-color","#000");
         $('.header-title_season-text').addClass("bgc-black");
         $('.header-version').css({'text-align': 'right'});
         $('.header-menu').addClass("menu-cecutiont");
-        //main
 
+        $('.header_logo').css({"fill" : "#000"});
+
+        $('.submenu').css({"background":"#000"});
+        $('.submenu_link').css({"color":"#fff"});
+
+
+        $('.c-button--bg').css({"width": "100%", "background-color": "#000", "color": "#fff", "text-align": "center"});
+
+
+        //main
         $('.collective_nav').addClass("bgc-black");
         $('.c-age-restrictions--bg').addClass("bgc-white color-black");
 
@@ -118,17 +121,6 @@ $(document).ready(function() {
         $('.footer-menu').toggleClass("l-full-width");
         $('.c-search_field').toggleClass("c-search_field--big");
         $('.footer-menu_item').toggleClass("l-full-width_item");
-
-
-
-        // $('#CecutientTop, .TopMenu').css("display","block");
-        // $('#content').css({"width":"100%","padding":"0px"});
-        // $('#content *').css({"background":"#fff","color":"#000"});
-        // $('#bottom, #bottom *').css({"background":"#000","color":"#fff"});
-        // $('#headerline').css({"color":"#fff","background":"#000","height":"30px"});
-        // $('.TopMenu').css({"border":"1px solid #000","paddingTop":"10px","paddingBottom":"10px","marginTop":"10px"});
-        // $('.TopMenu li a').css({"background":"none","paddingTop":"0px","color":"#000"});
-        // $('.appointments').html("Записаться");
         $.cookie("CecutientCookie", "on", {
             expires: 365,
             path: '/'
@@ -206,6 +198,7 @@ $(document).ready(function() {
 
 
 
+
             $.cookie("fonts", "big", {
                 expires: 365,
                 path: '/'
@@ -218,26 +211,37 @@ $(document).ready(function() {
     /*Функции изменения цветовой схема*/
     function WhiteStyle(){
         if ($.cookie("CecutientCookie")=="on"){
-            // $('body, html').css({"color":"#000","background":"#fff"});
-            // $('.main a,.header-menu a, p, ul, li' +
-            //     '.c-card_text, ' +
-            //     '.about_text, ' +
-            //     '.js-dotted, ' +
-            //     '.subscription_text, ' +
-            //     '.subscription_title,' +
-            //     '.news_post-ava-description,' +
-            //     '.news_title,' +
-            //     '.news_post-date, ' +
-            //     '.header *').css({"color" : "#000"});
 
+            // $('.submenu_link').css({"color":"#fff"});
 
-            $('.news, .subscription, .about_description').css({"border-color" : "#000"});
-            $('body, html, .hearder *, .main *, .footer *, .submenu a').css({"color":"#000"});
-            $('body, html').css({"background":"#fff"});
-            $('.header a').css({"color":"#000"});
+            // $('.news, .subscription, .about_description').css({"border-color" : "#000"});
+            // $('body, html, .hearder *, .main *, .footer *, .submenu a').css({"color":"#000"});
+            // $('body, html').css({"background-color":"#fff"});
+            // $('.header a').css({"color":"#000"});
 
+            $('body, html').css({"color":"#000","background":"#fff"});
+            $('.main a,.header-menu a, p, ul, li' +
+                '.c-card_text, ' +
+                '.about_text, ' +
+                '.js-dotted, ' +
+                '.subscription_text, ' +
+                '.subscription_title,' +
+                '.news_post-ava-description,' +
+                '.news_title,' +
+                '.news_post-date, ' +
+                '.header *').css({"color" : "#000"});
+            $('.header-title_season-text').css({"background":"#000","color":"#fff"});
+            $('.collective_nav, .collective_link').css({"background-color":"#000", "color":"#fff"});
 
-            // $('.header-title_season-text').css({"background":"#000","color":"#fff"});
+            $('.header_logo').css({"fill" : "#000"});
+            $('.header_logo-letter').css({"fill" : "#fff"});
+
+            $('.vk, .fb, .inst').css({"background-color":"#000"});
+            $('.c-button--bg').css({"width": "100%", "background-color": "#000", "color": "#fff", "text-align": "center"});
+
+            $('.submenu').css({"background":"#000"});
+            $('.menu-cecutiont .has-children::after').css({"border-bottom":"14px solid #000"});
+            $('.submenu_link').css({"color":"#fff"});
 
             $.cookie("style", "white", {
                 expires: 365,
@@ -249,28 +253,40 @@ $(document).ready(function() {
 
     function BlackStyle(){
         if ($.cookie("CecutientCookie")=="on"){
-            // $('body, html').css({"color":"#fff","background":"#000"});
+            $('body, html').css({"color":"#fff","background":"#000"});
             $('.l-layout').css({"background" : "transparent"});
             $('.header').css({"background" : "transparent"});
-            // $('.news, .subscription, .about_description').css({"border-color" : "#fff"});
-            // $('.main a,.header-menu a, p, ul, li' +
-            //     '.c-card_text, ' +
-            //     '.about_text, ' +
-            //     '.js-dotted, ' +
-            //     '.subscription_text, ' +
-            //     '.subscription_title,' +
-            //     '.news_post-ava-description,' +
-            //     '.news_title,' +
-            //     '.news_post-date, ' +
-            //     '.header *').css({"color" : "#fff"});
-            // $('.collective_nav, .collective_link, .submenu, .submenu a, .header-title_season-text').css({"background":"#fff", "color":"#000"});
-
             ////
-            $('.header a').css({"color":"#fff"});
-            $('.news, .subscription, .about_description').css({"border-color" : "#fff"});
-            $('body, html, .hearder *, .main *, .footer *, .submenu a').css({"color":"#fff"});
-            $('body, html').css({"background":"#000"});
+            // $('.header a').css({"color":"#fff"});
+            // $('.news, .subscription, .about_description').css({"border-color" : "#fff"});
+            // $('body, html, .hearder *, .main *, .footer *').css({"color":"#fff"});
+            // $('body, html').css({"background":"#000"});
 
+            $('.news, .subscription, .about_description').css({"border-color" : "#fff"});
+            $('.main a,.header-menu a, p, ul, li' +
+                '.c-card_text, ' +
+                '.about_text, ' +
+                '.js-dotted, ' +
+                '.subscription_text, ' +
+                '.subscription_title,' +
+                '.news_post-ava-description,' +
+                '.news_title,' +
+                '.news_post-date, ' +
+                '.header *').css({"color" : "#fff"});
+            $('.collective_nav, .collective_link, .header-title_season-text').css({"background":"#fff", "color":"#000"});
+
+            $('.header_logo').css({"fill" : "#fff"});
+            $('.header_logo-letter').css({"fill" : "#000"});
+
+            $('.vk, .fb, .inst').css({"background-color":"#000"});
+
+            $('.footer, .footer_body').css({"background-color":"#000"});
+
+            $('.c-button--bg').css({"width": "100%", "background-color": "#fff", "color": "#000", "text-align": "center"});
+
+            $('.submenu').css({"background":"#fff"});
+            $('.menu-cecutiont .has-children::after').css({"border-bottom":"14px solid #fff"});
+            $('.submenu_link').css({"color":"#000"});
 
             $.cookie("style", "black", {
                 expires: 365,
@@ -282,39 +298,51 @@ $(document).ready(function() {
 
     function BlueStyle(){
         if ($.cookie("CecutientCookie")=="on"){
-            // $('body, html').css("background","#9DD1FF");
+            $('body, html').css("background","#9DD1FF");
             $('.l-layout').css({"background" : "transparent"});
             $('.header').css({"background" : "transparent"});
 
-            $('.header a').css({"color":"#000"});
-            $('.news, .subscription, .about_description').css({"border-color" : "#000"});
-            $('body, html, .hearder *, .main *, .submenu a').css({"color":"#000"});
-            $('.footer *, .footer_body, .').css({"color":"#fff", "background-color" : "#03295e"});
-            $('body, html').css({"background":"#9DD1FF"});
-            // $('.footer, ' +
-            //     '.footer_body, ' +
-            //     '.footer_copiright, ' +
-            //     '.vk, ' +
-            //     '.fb, ' +
-            //     '.inst, ' +
-            //     '.header-title_season-text ').css({"background-color" : "#03295e"});
-
-
-
+            // $('.header a').css({"color":"#000"});
+            // $('.submenu').css({"background-color" : "#fff"});
             // $('.news, .subscription, .about_description').css({"border-color" : "#000"});
-            // $('.main a,.header-menu a, p, ul, li' +
-            //     '.c-card_text, ' +
-            //     '.about_text, ' +
-            //     '.js-dotted, ' +
-            //     '.subscription_text, ' +
-            //     '.subscription_title,' +
-            //     '.news_post-ava-description,' +
-            //     '.news_title,' +
-            //     '.news_post-date, ' +
-            //     '.header * ').css({"color" : "#03295e"});
+            // $('body, html, .hearder *, .main *').css({"color":"#000"});
+            // $('.footer *, .footer_body').css({"color":"#fff", "background-color" : "#03295e"});
+            // $('body, html').css({"background":"#9DD1FF"});
 
-            // $('.header-title_season-text').css({"color":"#fff"});
-            // $('.collective_nav, .collective_link, .submenu, .submenu a').css({"background":"#03295e", "color":"#fff"});
+
+            $('.footer, ' +
+                '.footer_body, ' +
+                '.footer_copiright, ' +
+                '.vk, ' +
+                '.fb, ' +
+                '.inst, ' +
+                '.header-title_season-text ').css({"background-color" : "#03295e"});
+
+
+
+            $('.news, .subscription, .about_description').css({"border-color" : "#000"});
+            $('.main a,.header-menu a, p, ul, li' +
+                '.c-card_text, ' +
+                '.about_text, ' +
+                '.js-dotted, ' +
+                '.subscription_text, ' +
+                '.subscription_title,' +
+                '.news_post-ava-description,' +
+                '.news_title,' +
+                '.news_post-date, ' +
+                '.header * ').css({"color" : "#03295e"});
+
+            $('.header-title_season-text').css({"color":"#fff"});
+            $('.collective_nav, .collective_link').css({"background":"#03295e", "color":"#fff"});
+
+            $('.header_logo').css({"fill" : "#03295e"});
+            $('.header_logo-letter').css({"fill" : "#fff"});
+
+            $('.c-button--bg').css({"width": "100%", "background-color": "#03295e", "color": "#fff", "text-align": "center"});
+
+            $('.submenu').css({"background":"#03295e"});
+            $('.menu-cecutiont .has-children::after').css({"border-bottom":"14px solid #03295e"});
+            $('.submenu_link').css({"color":"#fff"});
 
 
 
